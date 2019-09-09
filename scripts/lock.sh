149dabe9b3d1727bd6bb9962eda8c20f7926b753
lulivi <luislivilla@gmail.com>
2019-09-09 20:13:02 +0200

    new scrot functionality

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2018 Luis Liñán <luislivilla at gmail.com>
 # Dependences: scrot, imagemagick, i3lock
 
-/usr/bin/scrot /tmp/screenshot.png
+/usr/bin/scrot -o /tmp/screenshot.png
 /usr/bin/convert /tmp/screenshot.png \
 		 -sample 250 \
 		 -scale $(xdpyinfo | grep dimensions | grep -Po "[0-9]+x[0-9]+(?= pixels)")! \
