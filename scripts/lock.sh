3ac99d168862ed14d378c54d8fb4a2f19bbbf1d4
lulivi <luislivilla@gmail.com>
2019-11-01 18:34:09 +0100

    Modified lock script and added license header

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 # Copyright (c) 2018 Luis Liñán <luislivilla at gmail.com>
-# Dependences: scrot, imagemagick, i3lock
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This script uses i3lock to lock screen with a pixelized screenshot
+# 
+# Dependencies: scrot, imagemagick, i3lock
 
-res=""$(xdpyinfo | grep dimensions | grep -Po "[0-9]+x[0-9]+(?= pixels)")"!"
+# Obtain the screen resolution with xdpyinfo
+res=""$(xdpyinfo | awk '/dimensions:/ {print $2}')"!"
 
-/usr/bin/scrot -o /tmp/screenshot.png
-/usr/bin/convert /tmp/screenshot.png \
-		 -sample "${1:-100}" \
-		 -scale "$res" \
-		 /tmp/screenshotblur.png
-/usr/bin/i3lock -i /tmp/screenshotblur.png
+# Compute default sample value
+sample="$((${res%%x*} / 8))"
+
+# Capture screenshot
+/usr/bin/scrot -o "/tmp/screenshot.png"
+
+# Pixelizeit with imagemagik script
+/usr/bin/convert "/tmp/screenshot.png" \
+    -sample "${1:-$sample}" \
+    -scale "$res" \
+    "/tmp/screenshotblur.png"
+
+# Lock screen with the pixelized image
+/usr/bin/i3lock -i "/tmp/screenshotblur.png"
+
