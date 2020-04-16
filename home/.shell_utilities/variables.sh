e0aedeb38422c0207d2ae0a7b4ce90f6a864f3bc
lulivi <luislivilla@gmail.com>
2020-04-16 17:28:01 +0200

    Added bat as a man pager.

--- home/.shell_utilities/variables.sh
+++ home/.shell_utilities/variables.sh
@@ -6,4 +6,7 @@ export EDITOR='vim'
 export TERM='xterm-256color'
 export XDG_CONFIG_HOME="$HOME/.config"
 export SXHKD_SHELL='/usr/bin/sh'
+if [ "$(command -v bat)" ]; then
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+fi
 
