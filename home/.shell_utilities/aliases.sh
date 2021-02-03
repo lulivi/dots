2cfe22765e033ac40d7f126c7025774d0c5a4114
lulivi <luislivilla@gmail.com>
2021-02-03 23:58:51 +0100

    Add some aliases

--- home/.shell_utilities/aliases.sh
+++ home/.shell_utilities/aliases.sh
@@ -6,7 +6,7 @@
 alias please='sudo $(history -p !!)'
 alias restart_nm="systemctl restart NetworkManager.service"
 alias reload_bspwm="~/.config/bspwm/bspwmrc 2>&1 1>/dev/null"
-alias open="xdg-open"
+alias open="xdg-open &>/dev/null"
 
 ## Misc
 alias t="tmux"
@@ -19,6 +19,7 @@ alias graphic_card_in_use="glxinfo|egrep \"OpenGL vendor|OpenGL renderer*\""
 alias center_tmux="tmux split-window -h \; split-window -h \; swap-pane -s 1 -t 2 \; resize-pane -t1 -x 75 \; resize-pane -t3 -x 75 \; select-pane -t 2"
 alias walr="wal -R -o ~/.config/wal/reload_services.sh"
 alias myip="wget http://ipecho.net/plain -O - -q ; echo"
+alias spotify="ct Spotify; ncspot"
 
 # Entertainment
 alias matrix="cmatrix -b -a -u 2 -C yellow"
