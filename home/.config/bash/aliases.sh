1463e3354cb61fc53b4d1839b63e3d6358327310
lulivi <luislivilla@gmail.com>
2022-05-20 00:46:22 +0200

    Add docker clean

--- home/.config/bash/aliases.sh
+++ home/.config/bash/aliases.sh
@@ -25,6 +25,7 @@ alias spotify="cht Spotify; ncspot"
 alias sp="spotify"
 alias gitlab_run_job="gitlab-runner exec docker"
 alias plantuml_render="java -jar $HOME/.local/bin/plantuml.jar"
+alias docker_clean="docker system prune -a"
 
 # Entertainment
 alias matrix="cmatrix -b -a -u 2 -C yellow"
